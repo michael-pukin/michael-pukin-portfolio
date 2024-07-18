8dbd69efda6cd0969eb264ddc22c7df7fccfa401
michael-pukin <michael.pukin@gmail.com>
2024-07-18 16:27:33 -0400

    Changed copyright to self

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -129,7 +129,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Michael Pukin. Built with Docusaurus.`,
       },
       prism: {
         theme: prismThemes.github,
