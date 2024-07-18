0c022ae6c91e3978c2a7fd4f7d4424a9b34a2914
michael-pukin <michael.pukin@gmail.com>
2024-07-18 17:07:52 -0400

    removed Docs and Community sections in footer

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -90,33 +90,8 @@ const config = {
         style: 'dark',
         links: [
           
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Tutorial',
-                to: '/docs/intro',
-              },
-            ],
-          },
-          
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
-              },
-            ],
-          },
+
+
           {
             title: 'More',
             items: [
