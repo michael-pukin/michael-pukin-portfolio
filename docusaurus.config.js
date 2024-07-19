04e2310e422b7bdbc775a4428e68d6cd2a07c362
michael-pukin <michael.pukin@gmail.com>
2024-07-18 20:12:01 -0400

    Changed social card img

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -64,7 +64,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: 'img/lion-social-card.png',
       navbar: {
         title: 'Home',
         logo: {
